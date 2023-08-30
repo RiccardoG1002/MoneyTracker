24e9fe6feaca77c40c9c45f24dd26e9a9ac10f66
Riccardo Gallelli <riccardogallelli98@gmail.com>
2023-08-30 08:30:57 +0200

    customized error page and added navigation

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,27 @@
+import { Link, useNavigate, useRouteError } from "react-router-dom";
+
+// libraries
+import { HomeIcon, ArrowLeftIcon } from "@heroicons/react/24/solid";
+
 const Error = () => {
-  //TODO: customize this error component
-  return <div>Error</div>;
+  const error = useRouteError();
+  const navigate = useNavigate();
+
+  return (
+    <div className="error">
+      <h1>Oh oh! We've got a problem! 🚨</h1>
+      <h4>{error.message || error.statusText}</h4>
+      <div className="flex-md">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
+          <ArrowLeftIcon width={20}></ArrowLeftIcon>
+          <span>Go back</span>
+        </button>
+        <Link to="/" className="btn">
+          <HomeIcon width={20}></HomeIcon>
+          <span>Go home</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default Error;
